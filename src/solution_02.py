5b6403c8bbb5be6cbde6670701a400a16b64148c
Captainexpo-1 <captainexpo@gmail.com>
2024-12-02 00:14:51 -0500

    solution day #2

--- src/solution_02.py
+++ src/solution_02.py
@@ -16,15 +16,53 @@ import networkx
 def problem1(input: str) -> int | str:
     output: int = 0
     lines = utils.read_lines(input)
-    # Add your solution logic here
-    
+    for i in lines:
+        l = [int(j) for j in i.split(" ")]
+        inc = l[0] < l[1]
+        good = True
+        last = l[0]
+        for m in l[1:]:
+            if m < last and inc: 
+                good = False
+                break
+            elif inc == False and m > last:
+                good = False
+                break
+            if not (abs(last-m) > 0 and abs(last-m) <= 3):
+                good = False
+                break
+            last = m
+        output += 1 if good else 0
     return output
 
 def problem2(input: str) -> int | str:
     output: int = 0
     lines = utils.read_lines(input)
-    # Add your solution logic here
-    
+    for i in lines:
+        l = [int(j) for j in i.split(" ")]
+        def check(l):
+            inc = l[0] < l[1]
+            good = True
+            last = l[0]
+            for m in l[1:]:
+                if m < last and inc: 
+                    good = False
+                    break
+                elif inc == False and m > last:
+                    good = False
+                    break
+                if not (abs(last-m) > 0 and abs(last-m) <= 3):
+                    good = False
+                    break
+                last = m
+            return good
+        if check(l): 
+            output += 1
+        else: 
+            for idx in range(len(l)):
+                if check(l[:idx] + l[idx + 1:]):
+                    output += 1
+                    break
     return output
 
 if __name__ == "__main__":
