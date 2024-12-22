8134f262cd36d2b7219018dc84e9dc7c29ad5c8b
Captainexpo-1 <captainexpo@gmail.com>
2024-12-22 01:15:52 -0500

    day 22

--- src/solution_21.py
+++ src/solution_21.py
@@ -11,15 +11,105 @@ import itertools
 import collections
 import math
 import networkx
-
+import functools
 
 def problem1(input: str) -> int | str:
-    output: int = 0
     lines = utils.read_lines(input)
-    # Add your solution logic here
     
-    return output
+    keypad = {
+        '7': 0+0j,'8': 1+0j,'9': 2+0j,
+        '4': 0+1j,'5': 1+1j,'6': 2+1j,
+        '1': 0+2j,'2': 1+2j,'3': 2+2j,
+                  '0': 1+3j,'A': 2+3j,
+    }
+    
+    # make a graph out of the keypad using networkx
+    G_keypad = networkx.DiGraph()
+    for k,v in keypad.items():
+        G_keypad.add_node(v)
+    for k1,v1 in keypad.items():
+        for k2, v2 in keypad.items():
+            if k1 == k2: continue
+            if abs(v1-v2) == 1 or abs(v1-v2) == 1j:
+                G_keypad.add_edge(v1,v2)
+    
+    dirpad = {
+                   '^': 1+0j, 'A': 2+0j,
+        '<': 0+1j, 'v': 1+1j, '>': 2+1j,
+    }
+    
+    G_dirpad = networkx.Graph()
+    for k,v in dirpad.items():
+        G_dirpad.add_node(v)
+    for k1,v1 in dirpad.items():
+        for k2, v2 in dirpad.items():
+            if k1 == k2: continue
+            if abs(v1-v2) == 1 or abs(v1-v2) == 1j:
+                G_dirpad.add_edge(v1,v2)
+    
+    #print(G_dirpad.edges)
+    #print(G_keypad.edges)
+    
+    def get_path_as_chrs(path: list):
+        for idx in range(len(path)-1):
+            diff = path[idx+1] - path[idx]
+            if   diff ==   1: yield '>'
+            elif diff ==  -1: yield '<'
+            elif diff ==  1j: yield 'v'
+            elif diff == -1j: yield '^'
 
+    keypad_shortest_paths = collections.defaultdict(collections.defaultdict[list])
+    for k1,v1 in keypad.items():
+        for k2, v2 in keypad.items():
+            if k1 == k2: continue
+            path = networkx.shortest_path(G_keypad, v1, v2)
+            keypad_shortest_paths[k1][k2] = [''.join(i) for i in itertools.permutations(''.join(list(get_path_as_chrs(path))))]
+        keypad_shortest_paths[k1][k1] = 'A'
+        
+    dirpad_shortest_paths = collections.defaultdict(dict)
+    for k1,v1 in dirpad.items():
+        for k2, v2 in dirpad.items():
+            if k1 == k2: continue
+            path = networkx.shortest_path(G_dirpad, v1, v2)
+            dirpad_shortest_paths[k1][k2] = [''.join(i) for i in itertools.permutations(''.join(list(get_path_as_chrs(path))))]
+        dirpad_shortest_paths[k1][k1] = 'A'
+    
+    #print(dirpad_shortest_paths)
+    
+    @functools.cache
+    def get_robot_output(inp, robot):
+        shortest_paths = dirpad_shortest_paths
+        if robot == 1:
+            return inp
+        if robot == 0:
+            shortest_paths = keypad_shortest_paths
+        
+        pos = 'A'
+        
+        chunks = ["" for i in range(len(inp))]
+ 
+        for chunk_idx in range(len(chunks)):
+            i = inp[chunk_idx]
+            min_new_chunk = ""
+            min_new_len = math.inf
+            #print(pos, shortest_paths[pos], i, shortest_paths[pos].get(i))
+            for path in shortest_paths[pos][i]:
+                p = get_robot_output(path+"A", robot+1)
+                if len(p) < min_new_len:
+                    min_new_len = len(p)
+                    min_new_chunk = path+"A"
+            chunks[chunk_idx] = min_new_chunk
+
+            pos = i
+        
+        return get_robot_output(''.join(chunks), robot+1)
+    complexities = 0
+    for line in lines:
+       result = get_robot_output(line, 0)
+       complexities += len(result) * int(line[:-1].lstrip('0'))
+       print(line, result)
+    return complexities
+    
 def problem2(input: str) -> int | str:
     output: int = 0
     lines = utils.read_lines(input)
--- src/solution_22.py
+++ src/solution_22.py
@@ -15,19 +15,76 @@ import networkx
 
 def problem1(input: str) -> int | str:
     output: int = 0
-    lines = utils.read_lines(input)
-    # Add your solution logic here
+    vals = [int(i) for i in utils.read_lines(input)]
+
+    def get_next_num(num, n=0):
+        if n == 2000:
+            return num
+        
+        # num * 64 + mix + prune
+        # num // 32 + mix + prune
+        # num * 2048 + mix + prune
+        
+        mix_prune = lambda num, b: (num ^ b) % 16777216  
+        num = mix_prune(num, num*64)
+        num = mix_prune(num, num//32)
+        num = mix_prune(num, num*2048)
+        
+        return get_next_num(num, n+1)
     
-    return output
+    return sum(get_next_num(i) for i in vals)
 
 def problem2(input: str) -> int | str:
     output: int = 0
-    lines = utils.read_lines(input)
-    # Add your solution logic here
+    vals = [int(i) for i in utils.read_lines(input)]
+
+    def get_next_num(num):
+        for i in range(2000):
+            mix_prune = lambda num, b: (num ^ b) % 16777216  
+            num = mix_prune(num, num*64)
+            num = mix_prune(num, num//32)
+            num = mix_prune(num, num*2048)
+            yield num
     
-    return output
+    changes = {}
+    
+    prices = []
+    changes = []
+    for idx, i in enumerate(vals):
+        print(idx,"/",len(vals), end='\r')
+        prices.append([i]+[i%10 for i in get_next_num(i)])
+    # get each set of 4 changes
+    # e.g for prices = [1,3,4,9,2,3]
+    # changes = [[2,1,5,-7],[1,5,-7,2],[5,-7,2,1]]
+    for idx, p in enumerate(prices):
+        g = set()
+        c = []
+        for i in range(1,len(p)-4):
+            # append [(sequence, price at start of sequence, index of price)]
+            m = [p[idx]-p[idx-1] for idx in range(i, i+4)]
+            if str(m) not in g:
+                c.append((m, p[i+3], idx))
+                g.add(str(m))
+        changes.append(c)
+    print()
+    #for c in changes:
+        #print(list(filter(lambda x: x[0]==[-2,1,-1,3], c)))
+    
+    all_changes = utils.flatten(changes)
+    
+    possible_changes = collections.defaultdict(int)
+    for (c, price, idx) in all_changes:
+        possible_changes[str(c)] += price
+
+    m = -1
+    for k,v in possible_changes.items():
+        if v > m:
+            m = v
+    
+    return m
 
 if __name__ == "__main__":
+    sys.setrecursionlimit(10000)
     input_path = utils.get_input_file(Path(__file__).resolve().parent / "data", 22)
-    print(problem1(input_path))
+    #print(problem1(input_path))
     print(problem2(input_path))
