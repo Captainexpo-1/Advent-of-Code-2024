9c1ba962595bc8cc67b040f7788d8116e1a35a4e
Captainexpo-1 <captainexpo@gmail.com>
2024-12-23 01:29:25 -0500

    this is bs

--- src/solution_21.py
+++ src/solution_21.py
@@ -12,10 +12,12 @@ import collections
 import math
 import networkx
 import functools
+from pprint import pprint
+import json
 
 def precalculate_paths(keypad, dirpad):
     # make a graph out of the keypad using networkx
-    G_keypad = networkx.DiGraph()
+    G_keypad = networkx.Graph()
     for k,v in keypad.items():
         G_keypad.add_node(v)
     for k1,v1 in keypad.items():
@@ -40,34 +42,31 @@ def precalculate_paths(keypad, dirpad):
             elif diff ==  -1: yield '<'
             elif diff ==  1j: yield 'v'
             elif diff == -1j: yield '^'
-        yield 'A'
-
 
     keypad_shortest_paths = collections.defaultdict(collections.defaultdict[list])
     for k1,v1 in keypad.items():
         for k2, v2 in keypad.items():
             if k1 == k2: continue
-            path = networkx.all_shortest_paths(G_keypad, v1, v2)
+            path = networkx.shortest_path(G_keypad, v1, v2)
 
-            x = [''.join(list(get_path_as_chrs(i))) for i in path]
-            
+            x = ''.join(list(get_path_as_chrs(path)))
             
             keypad_shortest_paths[k1][k2] = x
-        keypad_shortest_paths[k1][k1] = 'A'
+        keypad_shortest_paths[k1][k1] = ""
         
     dirpad_shortest_paths = collections.defaultdict(dict)
     for k1,v1 in dirpad.items():
         for k2, v2 in dirpad.items():
             if k1 == k2: continue
-            path = networkx.all_shortest_paths(G_dirpad, v1, v2)
+            path = networkx.shortest_path(G_dirpad, v1, v2)
             
-            x = [''.join(list(get_path_as_chrs(i))) for i in path]
+            x = ''.join(list(get_path_as_chrs(path)))
             
             
             dirpad_shortest_paths[k1][k2] = x
-        dirpad_shortest_paths[k1][k1] = ['A']
+        dirpad_shortest_paths[k1][k1] = ""
     
-    return keypad_shortest_paths, dirpad_shortest_paths
+    return dict({k:dict(v) for k,v  in keypad_shortest_paths.items()}), dict(dirpad_shortest_paths)
 
 def problem1(input: str) -> int | str:
     lines = utils.read_lines(input)
@@ -85,35 +84,217 @@ def problem1(input: str) -> int | str:
         '<': 0+1j, 'v': 1+1j, '>': 2+1j,
     }
 
-    #print(G_dirpad.edges)
-    #print(G_keypad.edges)
+
     
-    keypad_shortest_paths, dirpad_shortest_paths = precalculate_paths(keypad, dirpad)
+    #keypad_shortest_paths, dirpad_shortest_paths = precalculate_paths(keypad, dirpad)
 
-    print(keypad_shortest_paths)
-    print(dirpad_shortest_paths)
 
-    def get_robot_output(inp, robot):
-        shortest_paths = dirpad_shortest_paths
-        if robot == 3:return inp
-        if robot == 0:shortest_paths = keypad_shortest_paths
+    keypad_shortest_paths = {
+        "7": {
+            "8": ">",
+            "9": ">>",
+            "4": "v",
+            "5": "v>",
+            "6": "v>>",
+            "1": "vv",
+            "2": "vv>",
+            "3": "vv>>",
+            "0": "vvv>",
+            "A": "vvv>>",
+            "7": ""
+        },
+        "8": {
+            "7": "<",
+            "9": ">",
+            "4": "<v",
+            "5": "v",
+            "6": "v>",
+            "1": "<vv",
+            "2": "vv",
+            "3": "vv>",
+            "0": "vvv",
+            "A": "vvv>",
+            "8": ""
+        },
+        "9": {
+            "7": "<<",
+            "8": "<",
+            "4": "<<v",
+            "5": "<v",
+            "6": "v",
+            "1": "<<vv",
+            "2": "<vv",
+            "3": "vv",
+            "0": "<vvv",
+            "A": "vvv",
+            "9": ""
+        },
+        "4": {
+            "7": "^",
+            "8": "^>",
+            "9": "^>>",
+            "5": ">",
+            "6": ">>",
+            "1": "v",
+            "2": "v>",
+            "3": "v>>",
+            "0": "vv>",
+            "A": "vv>>",
+            "4": ""
+        },
+        "5": {
+            "7": "<^",
+            "8": "^",
+            "9": "^>",
+            "4": "<",
+            "6": ">",
+            "1": "<v",
+            "2": "v",
+            "3": "v>",
+            "0": "vv",
+            "A": "vv>",
+            "5": ""
+        },
+        "6": {
+            "7": "<<^",
+            "8": "<^",
+            "9": "^",
+            "4": "<<",
+            "5": "<",
+            "1": "<<v",
+            "2": "<v",
+            "3": "v",
+            "0": "<vv",
+            "A": "vv",
+            "6": ""
+        },
+        "1": {
+            "7": "^^",
+            "8": "^^>",
+            "9": "^^>>",
+            "4": "^",
+            "5": "^>",
+            "6": "^>>",
+            "2": ">",
+            "3": ">>",
+            "0": "v>",
+            "A": "v>>",
+            "1": ""
+        },
+        "2": {
+            "7": "<^^",
+            "8": "^^",
+            "9": "^^>",
+            "4": "<^",
+            "5": "^",
+            "6": "^>",
+            "1": "<",
+            "3": ">",
+            "0": "v",
+            "A": "v>",
+            "2": ""
+        },
+        "3": {
+            "7": "<<^^",
+            "8": "<^^",
+            "9": "^^",
+            "4": "<<^",
+            "5": "<^",
+            "6": "^",
+            "1": "<<",
+            "2": "<",
+            "0": "<v",
+            "A": "v",
+            "3": ""
+        },
+        "0": {
+            "7": "^^^<",
+            "8": "^^^",
+            "9": "^^^>",
+            "4": "^^<",
+            "5": "^^",
+            "6": "^^>",
+            "1": "^<",
+            "2": "^",
+            "3": "^>",
+            "A": ">",
+            "0": ""
+        },
+        "A": {
+            "7": "^^^<<",
+            "8": "<^^^",
+            "9": "^^^",
+            "4": "^^<<",
+            "5": "<^^",
+            "6": "^^",
+            "1": "^<<",
+            "2": "<^",
+            "3": "^",
+            "0": "<",
+            "A": ""
+        }
+    }
+    # <v^>
+    dirpad_shortest_paths = {
+        "^": {
+            "A": ">",
+            "<": "<v",
+            "v": "v",
+            ">": "v>",
+            "^": ""
+        },
+        "A": {
+            "^": "<",
+            "<": "v<<",
+            "v": "<v",
+            ">": "v",
+            "A": ""
+        },
+        "<": {
+            "^": ">^",
+            "A": ">>^",
+            "v": ">",
+            ">": ">>",
+            "<": ""
+        },
+        "v": {
+            "^": "^",
+            "A": "^>",
+            "<": "<",
+            ">": ">",
+            "v": ""
+        },
+        ">": {
+            "^": "<^",
+            "A": "^",
+            "<": "<<",
+            "v": "<",
+            ">": ""
+        }
+    }
+
+
+
+
+    def get_robot_output(robot_num, cur_path):
+        if robot_num == 3:
+            return cur_path
+        paths = dirpad_shortest_paths if robot_num > 0 else keypad_shortest_paths
         
-        shortest_path = ""
-        shortest_len = math.inf
-        for start, end in zip(inp, inp[1:]):
-            all_paths = shortest_paths[start][end]
-            paths = []
-            for path in all_paths:
-                new_path = get_robot_output(path, robot+1)
-                
+        path = ""
+        for start, end in zip("A"+cur_path, cur_path):
+            #print(start, end, paths[start][end])
+            path += paths[start][end]+"A"
         
-        return get_robot_output(out, robot+1)
+        return get_robot_output(robot_num+1, path)
+        # get shortest path
+        
     
     complexities = 0
     for line in lines:
-       result = get_robot_output(line, 0)
+       result = get_robot_output(0, line)
        complexities += len(result) * int(line[:-1].lstrip('0'))
-       print(line, result, len(result))
+       print(line, result)
        
 #^A<<^^A>>AvvvA
        
